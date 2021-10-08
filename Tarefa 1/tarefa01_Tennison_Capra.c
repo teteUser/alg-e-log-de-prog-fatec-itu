0d8d08d7e20a1f25607d151efae251872d34202c
Tennison Capra <tennison_capra@hotmail.com>
2021-10-08 10:13:22 -0300

    primeira tarefa, primeiro commit
    Primeira tarefa de Algoritmos e Lógica de Programação da Fatec ITU

---
+++ Tarefa 1/tarefa01_Tennison_Capra.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Rascunho para comparação
+ 																  *
+                                                                 ***
+                                                                *****
+                                                               *******
+                                                              *********
+                                                             ***********
+                                                            *************
+                                                                  * 
+                                                                  * 
+                                                                  * 
+                                                                  * 
+                                                                  * 
+                                                                  * 
+                                                                  *
+*/
+// Tennison Capra
+
+int main() {
+	printf("\n\t     *\n\t    ***\n\t   *****\n\t  *******\n\t *********\n\t***********\n\t     *\n\t     *\n\t     *\n\t     *\n\t     *\n\t     *\n\t     *\n");
+	
+	return 0;
+}
+
+
+
