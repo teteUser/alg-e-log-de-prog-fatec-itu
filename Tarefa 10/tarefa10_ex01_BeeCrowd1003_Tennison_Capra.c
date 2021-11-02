5be3ad4a812b4ce0b1bd49cc980c23fe6a9d1b31
Tennison Capra <tennison_capra@hotmail.com>
2021-11-02 09:04:22 -0300

    Create tarefa10_ex01_BeeCrowd1003_Tennison_Capra.c

---
+++ Tarefa 10/tarefa10_ex01_BeeCrowd1003_Tennison_Capra.c
@@ -0,0 +1,42 @@
+// Tarefa 10 - Beecrowd # 1 - Ex 1003
+// Tennison Capra
+
+/*
+	Leia dois valores inteiros, no caso para variáveis A e B. A seguir,
+	calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável.
+	
+	Entrada
+	O arquivo de entrada contém 2 valores inteiros.
+	
+	Saída
+	Imprima a mensagem "SOMA" com todas as letras maiúsculas, com um espaço em branco antes e depois
+	da igualdade seguido pelo valor correspondente à soma de A e B. Como todos os problemas,
+	não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+
+
+	Exemplos
+	Entrada		Saída
+	30
+	10			SOMA = 40
+	
+	-30
+	10			SOMA = -20
+	
+	0
+	0			SOMA = 0	
+*/
+
+#include <stdio.h>
+
+int main(){
+	int A, B, SOMA;
+	
+	scanf("%i", &A);
+	scanf("%i", &B);
+
+	SOMA  = A + B;    
+
+	printf("SOMA = %i\n", SOMA);
+	return 0;
+}
+
